3b69acdd863e7f7f2565b4a39c257d075f1ed7ef
Islam Solaiman <islam.solaiman92@gmail.com>
2024-01-25 21:29:01 +0000

    0x03-log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,48 +3,35 @@
     A script for parsing HTTP request logs.
 '''
 
-import sys
-import re
+def print_statistics(total_size, status_code_counts):
+    """print"""
+    print(f"File size: {total_size}")
+    for code in sorted(status_code_counts):
+        print(f"{code}: {status_code_counts[code]}")
 
 
-def output(log: dict) -> None:
-    """
-    helper function to display stats
-    """
-    print("File size: {}".format(log["file_size"]))
-    for code in sorted(log["code_frequency"]):
-        if log["code_frequency"][code]:
-            print("{}: {}".format(code, log["code_frequency"][code]))
+line_number = 0
+codes = [200, 301, 400, 401, 403, 404, 405, 500]
+sumAll = 0
+times = []
+n_of_counts = {}
+try:
+    for line in sys.stdin:
+        args = line.split()
+        sumAll += int(args[-1])
+        times.append(int(args[-2]))
+        line_number += 1
 
-
-if __name__ == "__main__":
-    regex = re.compile(
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
-
-    line_count = 0
-    log = {}
-    log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0 for code in [
-            200, 301, 400, 401, 403, 404, 405, 500]}
-
-    try:
-        for line in sys.stdin:
-            line = line.strip()
-            match = regex.fullmatch(line)
-            if (match):
-                line_count += 1
-                code = match.group(1)
-                file_size = int(match.group(2))
-
-                # File size
-                log["file_size"] += file_size
-
-                # status code
-                if (code.isdecimal()):
-                    log["code_frequency"][code] += 1
-
-                if (line_count % 10 == 0):
-                    output(log)
-    finally:
-        output(log)
+        if line_number % 10 == 0:
+            for i in sorted(times):
+                if i is None or not isinstance(i, int):
+                    continue
+                if i in n_of_counts and i in codes:
+                    n_of_counts[i] += 1
+                else:
+                    n_of_counts[i] = 1
+            print_statistics(sumAll, n_of_counts)
+            times.clear()
+except KeyboardInterrupt:
+    print_statistics(sumAll, n_of_counts)
+    sys.exit(0)
